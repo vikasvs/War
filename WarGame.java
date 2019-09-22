89eca2302db27d9004710fd239245e8d5a04148a
Vikas Sharma <vvsharma21@berkeley.edu>
2019-09-21 19:46:04 -0700

    part 1

--- WarGame.java
+++ WarGame.java
@@ -1,9 +1,30 @@
 public class WarGame {
 	public static void main(String[] args){
-		List<Card> cardDeck = new ArrayList<Card>();
+		//Create a "deck"
+		List<Card> deck = new ArrayList<Card>();
 
-		
+		//Add cards to deck(52)
+		for (int suits=0; suits<4 suits++){
+			for (int ranks=0; ranks<15; rank++){
+				deck.add(new card(x,y));
+			}
+		}
 
+		/**
+		Logic
+		create 2 decks for each player
+		add 26 cards to each deck
+		play each card(maybe have user input saying "play card and have to type yes)
+		compare the rank between the two cards
+			higher rank wins both carrd
+			place card at bottom og deck
+			print who won the round
+		if ranks are equal than war occurs
+			make up rules for what happens when war occurs
+			do war
+			end war
+			print whoever wins the  when the cards are all gone for one person
+		*/
 
 	}
 }
